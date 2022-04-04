fbde4075178297bb8ac4d765d1bed3ac520b563e
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-04-04 23:08:26 +0300

    04.04.22

--- js/js.js
+++ js/js.js
@@ -14,8 +14,3 @@ const testObj = {
   "my side": "veggies",
   "the drink": "water",
 };
-
-// Only change code below this line
-const entreeValue = testObj["an entree"]; // Change this line
-const drinkValue = testObj["the drink"]; // Change this line
-////////////////////////////
